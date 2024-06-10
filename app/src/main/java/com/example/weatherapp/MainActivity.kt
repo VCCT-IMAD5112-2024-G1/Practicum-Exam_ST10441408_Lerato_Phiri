3ce993d3b8562b505587bae8ba5e8bc5494ea053
vicky <ST10441408@vcconnect.edu.za>
2024-06-10 08:55:37 +0200

    FINISHED PROJECTS

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.weatherapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainButton: Button = findViewById<Button>(R.id.mainButton)
+
+        // code to change from first to second page
+        mainButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainScreen::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
